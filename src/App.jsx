56c1019092f7d76575d4b23e058c43dffda2b715
Pablo Anjos <pablomatheussilvaanjoss@gmail.com>
2025-01-10 16:02:44 -0300

    Section de suporte add

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import { SmartWatch } from "./components/SmartWatch"
 import appleWatchSe from './assets/images/watch-se.png'
 import appleWatchSeries10 from './assets/images/watch-series-10.png'
 import appleWatchUltra2 from './assets/images/watch-ultra-2.png'
+import imgSuporte from './assets/images/suporte.png'
 
 
 
@@ -87,6 +88,17 @@ function App() {
                               price={"A partir de R$ 874,92"}
                               />
                       </div>
+          </section>
+
+          {/* SECTION SUPPORT */}
+          <section className="w-screen h-[662px] flex items-center justify-around">
+                    <div className="w-[510px] h-[371px] border-2 rounded-xl p-12 gap-5 flex flex-col">
+                          <h1 className="text-gray-300 text-3xl font-bold">24/7 no suporte ao cliente</h1>
+                          <p className="text-gray-300 text-lg">Nosso suporte está 7/7 24horas por dia para te ajudar com qualquer problema em nossos aparelhos</p>
+                          <button className="bg-gradient-to-l from-fuchsia-500 to-indigo-600 w-44 h-10 rounded-xl text-lg text-white mt-5">Nosso suporte</button>
+                    </div>
+
+                    <img src={imgSuporte} alt="Imagem dos atendentes do suporte" />
           </section>
     </>
   )
